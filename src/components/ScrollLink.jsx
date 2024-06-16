f9a22583194f671e0449c1b1dbb14540bb49f1b5
Simona Wurst <simona_wu@yahoo.de>
2024-06-16 12:40:55 +0200

    Add basic popup

--- src/components/ScrollLink.jsx
+++ src/components/ScrollLink.jsx
@@ -3,19 +3,17 @@ import { scroller } from 'react-scroll'
 
 
 const ScrollLink = ({ to, children }) => {
-  const handleClick = () => {
-    scroller.scrollTo(to.replace('#', ''), {
-      smooth: true,
-      offset: 30,
-    })
-  }
-
-  return (
-    <Link to={to} onClick={handleClick}>
-      {children}
-    </Link>
-  )
+    const handleClick = () => {
+        scroller.scrollTo(to.replace('#', ''), {
+            smooth: true,
+            offset: 30,
+        })
+    }
+    return (
+        <Link to={to} onClick={handleClick}>
+            {children}
+        </Link>
+    )
 }
-
 
 export default ScrollLink
--- src/components/Terminvereinbarung.jsx
+++ src/components/Terminvereinbarung.jsx
@@ -1,12 +1,30 @@
 import Calendar from "/img/Calendar.png"
+import { useState } from 'react'
 
 function Terminvereinbarung() {
+    const [showPopup, setShowPopup] = useState(false);
+    const handleButtonClick = () => {
+        setShowPopup(true)
+    }
+
+    const handlePopupClose = () => {
+        setShowPopup(false)
+    }
     return (
         <>
-            <button className="termin-sticky-button">
+            <button className="termin-sticky-button" onClick={handleButtonClick}>
                 Terminvereinbarung
                 <img src={Calendar} alt="Kalender Icon" />
             </button>
+            {showPopup && (
+                <div className="popup">
+                    <div className="popup-content">
+                    <h2>Terminvereinbarung</h2>
+                    <p>Hier gibt's noch nischts zu sehen, keine Input fields nix nada.</p>
+                    <button onClick={handlePopupClose}>Popup schließen</button>
+                </div>
+                </div>
+            )}
         </>
     )
 }
