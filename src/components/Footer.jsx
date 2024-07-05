19cf3fec0ad9fc79a1c80bc6de75f23355c0d1c1
Simona Wurst <simona_wu@yahoo.de>
2024-07-05 09:28:27 +0200

    Add footer info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,18 +4,35 @@ import { PiFlowerLotus } from "react-icons/pi"
 
 function Footer() {
     return (
-        <div id="footer" className="footer">
-            <div className="footer-logo">
-                <PiFlowerLotus size={50} color='white' />
+        <div id="footer">
+            <div className="footer">
+                <div>
+                    <h3>Where to find us</h3>
+                    <p>83 An Thượng 29</p>
+                    <p>Bắc Mỹ Phú</p>
+                </div>
+                <div>
+                    <h3>Your Contact</h3>
+                    <p>Sweetheart</p>
+                    <p>+49 1234 56789</p>
+                </div>
+                <div>
+                    <h3 className="footer-text">Join the Crew!</h3>
+                    <div className="footer-icons">
+                        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="footer-icon">
+                            <FaInstagram size={30} />
+                        </a>
+                        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="footer-icon">
+                            <FaFacebook size={30} />
+                        </a>
+                    </div>
+                </div>
             </div>
-            <h3 className="footer-text">Join the Crew!</h3>
-            <div className="footer-icons">
-                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="footer-icon">
-                    <FaInstagram size={30} />
-                </a>
-                <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="footer-icon">
-                    <FaFacebook size={30} />
-                </a>
+            <div className="footer-logo">
+                <div >
+                    <PiFlowerLotus size={50} color='white' />
+                </div>
+                <p>© 2024 - The Yoga Shala Company Inc.</p>
             </div>
         </div>
     )
